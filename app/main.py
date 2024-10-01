4f991af3ee6f0848f21b458d16f4b58c5e1af227
iwaggoner <iwaggoner95@gmail.com>
2024-09-30 20:43:22 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os, fnmatch
+import subprocess
     
 
 def print_type(subcommand, path):
@@ -17,6 +18,21 @@ def print_type(subcommand, path):
                 return print(subcommand, "is", text)
         sys.stdout.write(f"{subcommand}: not found\n")
 
+def check_program(program, path):
+    for x in path:
+        text = x + "/" + program.lower().split()[0]
+        if os.path.isfile(text):
+            return True
+    return False
+
+def run_program(program, path):
+    for x in path:
+        text = x + "/" + program.lower().split()[0]
+        if os.path.isfile(text):
+            os.system(text + " " + program.split()[1])
+            return 0
+    return 0
+
 def print_exit():
     sys.exit(0)
 
@@ -30,6 +46,7 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         command = input()
+        
 
         if command.lower().split()[0] == 'type':
             print_type(command[command.index(" ")+1:], PATH)
@@ -37,10 +54,13 @@ def main():
             print_exit()
         elif command.lower().split()[0] == 'echo':
             print_echo(command[command.index(" ")+1:])
+        elif check_program(command, PATH):
+            run_program(command, PATH)
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
     main()
+
 
 
